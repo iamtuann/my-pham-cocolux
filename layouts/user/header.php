bcc42bd4525b6b61d7e5a993b0a28d1f003b738e
nguyentuan <dungtuann67@gmail.com>
2024-05-04 16:46:52 +0700

    header user

---
+++ layouts/user/header.php
@@ -0,0 +1,37 @@
+<div class="top-header">
+  <div class="container">
+    <i class="fal fa-phone-alt"></i>
+    <span class="ms-2">+84 989 382 xxx</span>
+  </div>
+</div>
+<header class="header">
+  <div class="container">
+    <div class="d-flex justify-content-between align-items-center gx-4">
+      <a href="/">
+        <img src="assets/images/logo-full-black-cocolux.png" alt="">
+      </a>
+      <div class="d-flex align-items-center justify-content-end column-gap-4">
+        <form action="" name="search" method="GET" class="d-flex align-items-center">
+          <input type="text" class="search-input">
+          <button class="search-btn">
+            <i class="far fa-search"></i>
+          </button>
+        </form>
+        <a class="header-action" href="/cart">
+          <div class="position-relative">
+            <i class="fal fa-shopping-cart"></i>
+            <span class="count">1</span>
+          </div>
+          <span>Giỏ hàng</span>
+        </a>
+        <div class="header-action" href="/cart">
+          <i class="fal fa-user-headset"></i>
+          <span>Hỗ trợ</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+<div class="nav-bar">
+
+</div>
