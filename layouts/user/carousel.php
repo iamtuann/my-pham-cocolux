897b75de58ee83693c6908168ba8170d2df77839
duwj1211 <tranvandu1211bg@gmail.com>
2024-05-18 16:33:51 +0700

    Du commit

---
+++ layouts/user/carousel.php
@@ -0,0 +1,35 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<div id="carouselExampleIndicators" class="carousel slide container" data-ride="carousel">
+    <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="https://cocolux.com/storage/upload_image/images/banner-web/banner-trang-chu-nho.jpg" class="d-block w-100" alt="Image 1" style="width: 100%;
+    height: 550px;">
+        </div>
+        <div class="carousel-item">
+            <img src="https://cocolux.com/storage/upload_image/images/Banner/freeship-cocolux.jpeg" class="d-block w-100" alt="Image 2" style="width: 100%;
+    height: 550px;">
+        </div>
+        <div class="carousel-item">
+            <img src="https://cocolux.com/storage/slider/larger/1714617814-banner-web-sale-tong-thang-5.webp" class="d-block w-100" alt="Image 3" style="width: 100%;
+    height: 550px;">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
---
+++ layouts/user/list_brand.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="assets/css/user/list_brand.css">
+
+<?php
+	$sql_show_brand = "SELECT * FROM brand LIMIT 8";
+    $query_show_brand = mysqli_query($connect, $sql_show_brand);
+?>
+<div class="container">
+	<div class="wp-brands">
+		<div class="d-flex justify-content-between">
+			<h3 class="title">THƯƠNG HIỆU NỔI BẬT</h3>
+			<a class="view-all" href="brands">XEM TẤT CẢ</a>
+		</div>
+		<div class="list-brands">
+			<?php
+			while ($row2 = mysqli_fetch_array($query_show_brand)) {
+			?>
+				<div class="wp-brand-item">
+					<a style="text-decoration: none;" href="">
+						<img class="brand-img" src=<?= $row2['image'] ?>>
+						<div class="brand-name"><?= $row2['name'] ?></div>
+					</a>
+				</div>
+			<?php
+			}
+			?>
+		</div>
+	</div>
+</div>
