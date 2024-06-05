95675b3b3b6904817326e3edc5148911e7c7dbdf
nguyentuan <dungtuann67@gmail.com>
2024-06-05 16:50:53 +0700

    fix logout admin

--- layouts/admin/header.php
+++ layouts/admin/header.php
@@ -3,6 +3,6 @@
     <div class="">
         <span>Admin
             <?= isset($_SESSION["name"]) && $_SESSION["role"] == "admin" ? $_SESSION["name"] : ""  ?></span>
-        <a class="btn btn-primary mx-2" href="http://localhost/my-pham-cocolux/login.php?log-out=1">Đăng xuất</a>
+        <a class="btn btn-primary mx-2" href="../login.php?log-out=1">Đăng xuất</a>
     </div>
 </div>
